af5f7ce2e0730533a837f91691659b20313ad072
glen <ellisonglenmay@gmail.com>
2024-10-13 23:35:12 -0230

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import db from '@astrojs/db';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), alpinejs()]
 });
--- db/seed.ts
+++ db/seed.ts
@@ -2,16 +2,9 @@ import { db, Movies, Ratings, Viewers } from 'astro:db';
 import { moviesData, ratingsData, viewersData } from './data';
 
 export default async function () {
-
-	// // drop tables
-	// await db.delete(Movies).run();
-	// await db.delete(Ratings).run();
-	// await db.delete(Viewers).run();
-	
-	
-  	await db.insert(Movies).values(moviesData);
-	  await db.insert(Viewers).values(viewersData)
-	  await db.insert(Ratings).values(ratingsData)
+	await db.insert(Movies).values(moviesData);
+	await db.insert(Viewers).values(viewersData)
+	await db.insert(Ratings).values(ratingsData)
 }
 
 
