490829c0a2a9119ca977f858cb9c229bd882643e
glen <ellisonglenmay@gmail.com>
2024-10-14 23:04:41 -0230

    continued fleshing out of content-collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,28 @@
 import { defineCollection, z } from 'astro:content';
 import { db, MoviesDB, RatingsDB, ViewersDB } from 'astro:db';
 
+// Todo: Move to API
 const movies = await db.select().from(MoviesDB);
 const ratings = await db.select().from(RatingsDB);
 const viewers = await db.select().from(ViewersDB);
 
+// Each loader function returns an array of objects that match the schema
+// Collections are defined with a loader function and a schema
+// We resolve foreign keys and map data to include related data at this level
+// We can write APIs to hit the database directly for more complex queries
+
+// ** Movies Collection **
+// /movies/[id]
 const Movies = defineCollection({ 
  loader: async () => {
 
+    // Map movies to include ratings
     const moviesWithRatings = movies.map((movie) => {
       const movieRatings = ratings.filter((rating) => rating.movieId === movie.id);
       const totalRatings = movieRatings.length;
       const average = totalRatings > 0 ? movieRatings.reduce((sum, rating) => sum + rating.score, 0) / totalRatings : 0;
 
+      // Map ratings to include viewer name
       const ratingsWithViewers = movieRatings.map((rating) => {
         const viewer = viewers.find((viewer) => viewer.id === rating.viewerId);
         return {
@@ -24,6 +34,7 @@ const Movies = defineCollection({
         };
       });
       
+      // Return movie with ratings
       return {
         ...movie,
         id: movie.id.toString(),
@@ -36,6 +47,7 @@ const Movies = defineCollection({
     return moviesWithRatings;
   },
 
+  // SCHEMA
   schema: z.object({
     id: z.string(),
     title: z.string(),
@@ -52,9 +64,10 @@ const Movies = defineCollection({
   }),
 });
 
+// ** Ratings Collection **
+// /ratings/[id]
 const Ratings = defineCollection({
   loader: async () => {
-    const ratings = await db.select().from(RatingsDB);
     return ratings.map((rating) => ({
       id: rating.id.toString(),
       movieId: rating.movieId,
@@ -62,6 +75,8 @@ const Ratings = defineCollection({
       rating: rating.score,
     }));
   },
+
+  // SCHEMA
   schema: z.object({
     id: z.string(),
     movieId: z.number(),
@@ -70,22 +85,46 @@ const Ratings = defineCollection({
   }),
 });
 
+// ** Viewers Collection **
+// /viewers/[id]
 const Viewers = defineCollection({
   loader: async () => {
-    // const viewers = await db.select().from(ViewersDB);
-    return viewers.map((viewer) => ({
-      id: viewer.id.toString(),
-      name: viewer.name,
-    }));
+
+    const viewersWithRatings = viewers.map((viewer) => {
+      // Filter ratings for this viewer
+      const viewerRatings = ratings.filter((rating) => rating.viewerId === viewer.id);
+      // Map ratings to include movie title
+      const ratingsWithMovies = viewerRatings.map((rating) => {
+        const movie = movies.find((movie) => movie.id === rating.movieId);
+        return {
+          id: rating.id,
+          movieId: rating.movieId,
+          movieTitle: movie ? movie.title : 'Unknown',
+          score: rating.score,
+        };
+      });
+
+      return {
+        ...viewer,
+        id: viewer.id.toString(),
+        ratings: ratingsWithMovies,
+      };
+    });
+
+    return viewersWithRatings;
   },
 
+  // SCHEMA
   schema: z.object({
     id: z.string(),
     name: z.string(),
+    ratings: z.array(z.object({
+      id: z.number(),
+      movieId: z.number(),
+      movieTitle: z.string(),
+      score: z.number(),
+    })),
   }),
 });
-
-
-
 
 export const collections = { Movies, Ratings, Viewers };
