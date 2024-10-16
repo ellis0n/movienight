8747037e25a1fa35bd1a6409008a9a3a035a3000
glen <ellisonglenmay@gmail.com>
2024-10-16 20:05:07 -0230

    initial clerk and vercel serverless rigging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import db from '@astrojs/db';
-
-import alpinejs from '@astrojs/alpinejs';
-
+import vercel from '@astrojs/vercel/serverless';
+import clerk from '@clerk/astro';
 
 // https://astro.build/config
-export default defineConfig(
-  {
-    experimental: {
-      contentLayer: true
+export default defineConfig({
+  experimental: {
+    contentLayer: true,
   },
-  integrations: [tailwind(), db(), alpinejs()]
+  integrations: [tailwind(), db(), clerk()],
+  output: 'server',
+  adapter: vercel(),
 });
