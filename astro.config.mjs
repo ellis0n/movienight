ea7d5c94f69c2d63e6542cfe74d9034bbfe7dd52
glen <ellisonglenmay@gmail.com>
2024-10-23 21:00:58 -0230

    sidebar open fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import db from '@astrojs/db';
 import vercel from '@astrojs/vercel/serverless';
 import clerk from '@clerk/astro';
 
+import vtbot from 'astro-vtbot';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     contentLayer: true,
   },
-  integrations: [tailwind(), db(), clerk()],
+  integrations: [tailwind(), db(), clerk(), vtbot()],
   output: 'server',
   adapter: vercel(),
 });
